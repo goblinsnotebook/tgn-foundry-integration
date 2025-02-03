c2ffa81243ce648227b2a45c2a2f6c73752965bd
goblinsnotebook <contact@the-goblin.net>
2025-02-03 12:03:56 +0000

    tidy up

--- scripts/connection.js
+++ scripts/connection.js
@@ -1,3 +1,19 @@
+function readCookie(name) {
+    var nameEQ = name + '='
+    var ca = document.cookie.split(';')
+
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i]
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1, c.length)
+        }
+        if (c.indexOf(nameEQ) === 0) {
+            return c.substring(nameEQ.length, c.length)
+        }
+    }
+
+    return null
+}
 function getQueryStringByName(name, url = window.location.href) {
     name = name.replace(/[\[\]]/g, '\\$&');
     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
